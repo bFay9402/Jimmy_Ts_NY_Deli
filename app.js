1428dd7ecc9272b7d090d437b8b02aa16fd49733
bFay9402 <bfay9402@gmail.com>
2020-10-30 12:18:49 -0400

    added scrollspy, location map

---
+++ app.js
@@ -0,0 +1,16 @@
+const scrollSpy = () => {
+  const sections = document.querySelectorAll('.scrollspy');
+
+  window.onscroll = () => {
+    const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (let s in sections)
+      if (sections.hasOwnProperty(s) && sections[s].offsetTop <= scrollPos) {
+        const id = sections[s].id;
+        document.querySelector('.active').classList.remove('active');
+        document.querySelector(`a[href*=${ id }]`).parentNode.classList.add('active');
+      }
+  }  
+}
+
+scrollSpy();
